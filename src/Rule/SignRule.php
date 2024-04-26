ed97f2116105c032aee39137485fb0e360028be1
Andy Kirk <andy@andykirk.net>
2024-04-26 15:07:56 +0100

    J4

---
+++ src/Rule/SignRule.php
@@ -0,0 +1,126 @@
+<?php
+namespace NPEU\Module\Signpost\Site\Rule;
+
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+
+use SVG\SVG;
+use SVG\Nodes\Shapes\SVGRect;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Form\FormRule;
+use Joomla\CMS\Form\Rule\OptionsRule;
+use Joomla\Registry\Registry;
+
+defined('_JEXEC') or die;
+
+
+/**
+ * The ExecutionRulesRule Class.
+ * Validates execution rules, with input for other fields as context.
+ *
+ * @since  4.1.0
+ */
+class SignRule extends FormRule
+{
+    /**
+     * @var string  RULE_TYPE_FIELD   The field containing the rule type to test against
+     * @since  4.1.0
+     */
+    private const RULE_TYPE_FIELD = "sign.rule-type";
+
+    /**
+     * @var string CUSTOM_RULE_GROUP  The field group containing custom execution rules
+     * @since  4.1.0
+     */
+    private const CUSTOM_RULE_GROUP = "sign.custom";
+
+
+    public function tmpErrorHandler($errno, $errstr, $errfile, $errline) {
+        if (E_RECOVERABLE_ERROR === $errno) {
+            throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+        }
+        return false;
+    }
+
+    /**
+     * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form
+     *                                       field object.
+     * @param   mixed              $value    The form field value to validate.
+     * @param   ?string            $group    The field name group control value. This acts as an array container for the
+     *                                       field. For example if the field has `name="foo"` and the group value is set
+     *                                       to "bar" then the full field name would end up being "bar[foo]".
+     * @param   ?Registry          $input    An optional Registry object with the entire data set to validate against
+     *                                       the entire form.
+     * @param   ?Form              $form     The form object for which the field is being tested.
+     *
+     * @return boolean
+     *
+     * @since  4.1.0
+     */
+    public function test(\SimpleXMLElement $element, $value, $group = null, Registry $input = null, Form $form = null): bool
+    {
+        $values = (array) $value;
+
+        foreach ($values as $name => $v) {
+            #echo '<pre>'; var_dump($v); echo '</pre>';
+            // Validate SVG:
+            if (!empty($v->svg)) {
+                $svg_is_valid = true;
+                set_error_handler([$this, 'tmpErrorHandler']);
+                try {
+                    $image = @SVG::fromString($v->svg);
+                } catch(Exception $e) {
+                    Factory::getApplication()->enqueueMessage('One or more of the SVGs is invalid', 'error');
+                    $svg_is_valid = false;
+                }
+                restore_error_handler();
+
+                if (!$svg_is_valid) {
+                    return false;
+                }
+            }
+        }
+
+        /*$data = Factory::getApplication()->input->get('jform', array(), 'post', 'array');
+        #echo '<pre>'; var_dump($data); echo '</pre>'; exit;
+
+        $path = JPATH_ROOT . '/' . $data['uploadfolder'] . '/' . $data['csvfolder'];
+        if (!file_exists($path)) {
+            mkdir($path);
+        }
+        return true;*/
+
+        /*$fieldName = (string) $element['name'];
+        $ruleType  = $input->get(self::RULE_TYPE_FIELD);
+
+        if ($ruleType === $fieldName || ($ruleType === 'custom' && $group === self::CUSTOM_RULE_GROUP)) {
+            return $this->validateField($element, $value, $group, $form);
+        }
+
+        return true;*/
+    }
+
+    /**
+     * @param   \SimpleXMLElement  $element  The SimpleXMLElement for the field.
+     * @param   mixed              $value    The field value.
+     * @param   ?string            $group    The form field group the element belongs to.
+     * @param   Form|null          $form     The Form object against which the field is tested/
+     *
+     * @return boolean  True if field is valid
+     *
+     * @since  4.1.0
+     */
+    /*private function validateField(\SimpleXMLElement $element, $value, ?string $group = null, ?Form $form = null): bool
+    {
+        $elementType = (string) $element['type'];
+
+        // If element is of cron type, we test against options and return
+        if ($elementType === 'cron') {
+            return (new OptionsRule())->test($element, $value, $group, null, $form);
+        }
+
+        // Test for a positive integer value and return
+        return filter_var($value, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1]]);
+    }*/
+}
--- vendor/twig/twig/src/Node/Node.php
+++ vendor/twig/twig/src/Node/Node.php
@@ -27,7 +27,6 @@ class Node implements \Countable, \IteratorAggregate
     protected $lineno;
     protected $tag;
 
-    private $name;
     private $sourceContext;
 
     /**
@@ -40,7 +39,7 @@ class Node implements \Countable, \IteratorAggregate
     {
         foreach ($nodes as $name => $node) {
             if (!$node instanceof self) {
-                throw new \InvalidArgumentException(sprintf('Using "%s" for the value of node "%s" of "%s" is not supported. You must pass a \Twig\Node\Node instance.', \is_object($node) ? \get_class($node) : (null === $node ? 'null' : \gettype($node)), $name, \get_class($this)));
+                throw new \InvalidArgumentException(sprintf('Using "%s" for the value of node "%s" of "%s" is not supported. You must pass a \Twig\Node\Node instance.', \is_object($node) ? \get_class($node) : (null === $node ? 'null' : \gettype($node)), $name, static::class));
             }
         }
         $this->nodes = $nodes;
@@ -56,7 +55,7 @@ class Node implements \Countable, \IteratorAggregate
             $attributes[] = sprintf('%s: %s', $name, str_replace("\n", '', var_export($value, true)));
         }
 
-        $repr = [\get_class($this).'('.implode(', ', $attributes)];
+        $repr = [static::class.'('.implode(', ', $attributes)];
 
         if (\count($this->nodes)) {
             foreach ($this->nodes as $name => $node) {
@@ -105,7 +104,7 @@ class Node implements \Countable, \IteratorAggregate
     public function getAttribute(string $name)
     {
         if (!\array_key_exists($name, $this->attributes)) {
-            throw new \LogicException(sprintf('Attribute "%s" does not exist for Node "%s".', $name, \get_class($this)));
+            throw new \LogicException(sprintf('Attribute "%s" does not exist for Node "%s".', $name, static::class));
         }
 
         return $this->attributes[$name];
@@ -129,7 +128,7 @@ class Node implements \Countable, \IteratorAggregate
     public function getNode(string $name): self
     {
         if (!isset($this->nodes[$name])) {
-            throw new \LogicException(sprintf('Node "%s" does not exist for Node "%s".', $name, \get_class($this)));
+            throw new \LogicException(sprintf('Node "%s" does not exist for Node "%s".', $name, static::class));
         }
 
         return $this->nodes[$name];
@@ -145,6 +144,10 @@ class Node implements \Countable, \IteratorAggregate
         unset($this->nodes[$name]);
     }
 
+    /**
+     * @return int
+     */
+    #[\ReturnTypeWillChange]
     public function count()
     {
         return \count($this->nodes);
